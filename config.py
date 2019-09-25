762146c9b5df838940102de30efecb41f90d2932
honorine1 <uwurukundoh@gmail.com>
2019-09-25 19:23:48 +0200

    pitches are displayed

---
+++ config.py
@@ -0,0 +1,53 @@
+import os
+
+class Config:
+
+    # MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    # MOVIE_API_KEY ='ff73536b7f0e63d45fe4f61ecf8eb703'
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'hono1'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:  @localhost/pitching'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME ='uwurukundoh@gmail.com'
+    # MAIL_PASSWORD ='uwurukundo1993'
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:  @localhost/pitch_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:  @localhost/pitching'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,10 @@
+# export MOVIE_API_KEY=0a05ea57a37e91924e9ee789ef7f8fa9
+export DATABASE_URL='postgres://xkoszvcfcdlkrk:9d454a4207418f7d6084c0af1fa0c93a32656ec9783bef5ec6c86212ba57132a@ec2-54-235-240-126.compute-1.amazonaws.com:5432/d18732jk577r6a'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:  @localhost/pitching'
+
+export SECRET_KEY="hono1"
+# export SECRET_KEY="57hsa68i98ug2pi8532"
+# export MAIL_USERNAME='uwurukundoh@gmail.com'
+# export MAIL_PASSWORD='uwurukundo1993'
+python3.6 manage.py server
+
